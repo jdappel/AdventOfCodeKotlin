f300fd828fb2ba8224c542f84c83f6c544e422f2
Jeremy D. Appel <jeremy.d.appel@jpmorgan.com>
2022-12-08 09:56:34 -0500

    Day One complete

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
+import kotlin.math.max
+
 fun main() {
+
+    fun logic(input: List<String>): Map<Int,Int> {
+        var elves = mutableMapOf<Int,Int>()
+        var tmp = 0
+        var cnt = 0
+        input.forEach { line ->
+            if (line.isNotEmpty()) {
+                tmp += line.toInt()
+            }
+            else {
+                elves[++cnt] = tmp
+                tmp = 0
+            }
+        }
+        return elves
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return logic(input).maxBy { entry -> entry.value }.value
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return logic(input).entries.sortedByDescending { entry -> entry.value }.take(3).sumOf { it.value }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    //check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01_test")
     println(part1(input))
     println(part2(input))
 }
